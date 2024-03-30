a55add0c6fa3206e275a945601ed78729657583d
DhiegoVale <fernando>
2024-03-30 15:16:26 -0300

    transformando codigos em functions

--- ex005-antecessor e sucessorr.py
+++ ex005-antecessor e sucessorr.py
@@ -1,4 +1,12 @@
 ''' Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.'''
+def anterior_sucessor(n):
+    ant = n - 1
+    suc = n + 1
+    return ant, suc
+
+
 numero = int(input('Digite um número:'))
-print('o antecessor de {} é {}'.format(numero, (numero-1)))
-print('o sucessor de {} é {}'.format(numero,numero+1))
+anterior , sucessor = anterior_sucessor(numero)
+
+print(f'anterior = {anterior}')
+print(f'sucessor = {sucessor}')
