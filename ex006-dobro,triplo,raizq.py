ccd4f5e04dc13129f5ba4ad3ff84ae1394c249d5
DhiegoVale <fernando>
2024-03-31 10:03:27 -0300

    cont. functions transformations

--- ex006-dobro,triplo,raizq.py
+++ ex006-dobro,triplo,raizq.py
@@ -1,6 +1,15 @@
 '''Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.'''
+def fazer_operacoes(n):
+    d = 2 * n
+    t = 3 * n
+    r = n ** (1/2)
+    return d, t, r
+
+
 numero = int(input('digite um numero: '))
-print('o dobro de {} é {}'.format(numero, numero * 2))
-print('o triplo de {} é {}'.format(numero, numero * 3))
-print('a raiz quadrada de {} é {:.2f}'.format(numero, numero**(1/2)))
+dobro, triplo ,raizq = fazer_operacoes(numero)
+print(f'o dobro de {numero} é {dobro} ')
+print(f'o triplo de {numero} é {triplo} ')
+print(f'a raiz quadrada de {numero} é {raizq} ')
+
 
