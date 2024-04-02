4fe4454992e26e93899b3d8d40d2eec9191979d3
fernando vale <vallesilva248@gmail.com>
2024-04-02 17:32:07 -0300

    att coddigos para funcoes

--- ex007-media aritmetica.py
+++ ex007-media aritmetica.py
@@ -1,12 +1,12 @@
 '''Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.'''
 def calcular_media(n1,n2):
-    media = (n1+ n2) / 2
-    return media
+    #media = (n1+ n2) / 2
+    return (n1 + n2)/2
     
     
     
 nota1 = float(input('nota 1 : '))
 nota2 = float(input('nota 2 : '))
-print(f'media: {calcular_media(nota1,nota2):.2f}')
+print(f'media: {calcular_media(nota1,nota2):.1f}')
 
 
--- ex008-conversor de medidas.py
+++ ex008-conversor de medidas.py
@@ -1,15 +1,20 @@
 '''Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.'''
+def conversor_metros(m):
+    km = m / 1000
+    hm = m / 100
+    dam = m / 10
+    dm = m * 10
+    cm = m * 100
+    mm = m * 1000
+    return km,hm,dam,dm,cm,mm
+
 metro = float(input('valor em metros [M]: '))
-km = metro/1000
-hm = metro/100
-dam = metro/10
-dm = metro*10
-cm = metro*100
-mm = metro*1000
-print('{}m corresponde a: '.format(metro))
-print('{}km.'.format(km))
-print('{}hm.'.format(hm))
-print('{}dam.'.format(dam))
-print('{}dm.'.format(dm))
-print('{}cm.'.format(cm))
-print('{}mm.'.format(mm))
+km,hm,dam,dm,cm,mm = conversor_metros(metro)
+
+print('{:.0f}m corresponde a: '.format(metro))
+print('{:.0f}km.'.format(km))
+print('{:.0f}hm.'.format(hm))
+print('{:.0f}dam.'.format(dam))
+print('{:.0f}dm.'.format(dm))
+print('{:.0f}cm.'.format(cm))
+print('{:.0f}mm.'.format(mm))
